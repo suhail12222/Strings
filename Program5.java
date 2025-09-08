fb021f8abb653b4c6bcce86152b56abb8ec6da35
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-09-08 16:14:27 +0530

    checking the questions of stringbuffer string

---
+++ Program5.java
@@ -0,0 +1,17 @@
+package Strings;
+//StringBuffer sb = new
+//StringBuffer("JavaIsFun");
+//
+//sb.delete(4, 6);
+//
+//System.out.println(sb);
+public class Program5 {
+    public static void main(String[] args) {
+        StringBuffer sb = new
+                StringBuffer("JavaIsFun");
+
+        sb.delete(4, 6);
+
+        System.out.println(sb);//
+    }
+}
