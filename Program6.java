d088d5d64e63277aaf83d624ce138ea77431b8eb
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-09-08 16:16:15 +0530

    string buffer clear concepts

---
+++ Program6.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Program6 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+//        sb.append(null);
+
+        System.out.println(sb);//compile error because null is not a string
+    }
+}
